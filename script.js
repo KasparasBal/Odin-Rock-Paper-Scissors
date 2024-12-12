3eb200757f78c00362d55ec190e6ab80c843797b
Kasparas <KasparasBalys@gmail.com>
2024-12-12 17:26:42 +0200

    Added image change logic, score logic, additional card tuck animation

--- script.js
+++ script.js
@@ -4,14 +4,27 @@ const playButton = document.querySelector('.play_btn');
 const replayButton = document.querySelector('.replay_btn');
 const replaySection = document.querySelector('.game_replay_section')
 const replayOverlay = document.querySelector('.game_replay_overlay');
-const closeButton = document.querySelector('.close_btn');
+
+const gameCards = document.querySelector('.game_cards');
 
 const loadingEl = document.querySelector('.loading');
 
-const closeOverlay  = [replayButton, closeButton, replayOverlay];
+const winnerAnnouncement = document.querySelector('.winner_announcement');
+
+const battleArea = document.querySelector('.game_battle_area');
+
+const playerScoreOutput = document.querySelector('.game_player_score');
+const computerScoreOutput = document.querySelector('.game_computer_score');
+
+const humanSelection = document.querySelector('.humanSelection');
+const computerSelection = document.querySelector('.computerSelection');
 
 let loading = false;
-let interval = '';
+let loadloadInterval = '';
+let resultTimeout = '';
+
+let playerScore = 0;
+let computerScore = 0;
 
 playButton.addEventListener('click', (e) => {
 e.preventDefault();
@@ -22,18 +35,20 @@ loadingLogic();
 loadingEl.classList.remove('hidden');
 })
 
-closeOverlay.forEach(button => {
-    button.addEventListener('click', (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        
-        replaySection.classList.add('hidden');
-        })
-})
+
+replayButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    replaySection.classList.add('hidden');
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreOutput.textContent = 0;
+    computerScoreOutput.textContent = 0;
+    })
+
 
 const loadingLogic = () => {
     if(loading == true){
-        interval = setInterval(() => {
+        loadInterval = setInterval(() => {
            if(loadingEl.textContent != 'Players picking...'){
             loadingEl.textContent += '.';
            } else {
@@ -42,69 +57,104 @@ const loadingLogic = () => {
       }, 500);  // Change every 500ms
     
     } else {
-        clearInterval(interval);
+        clearInterval(loadInterval);
         loadingEl.textContent = 'Players picking'
     }
 }
 
 
+//PC CHOICE
 
-// let playerScore = 0;
-// let computerScore = 0;
-// let computerChoice = '';
-// let playerChoice = '';
-
-// const getComputerChoice = () => {
-//   const randomNumber =  Math.floor(Math.random()* 3) + 1;
+const getComputerChoice = () => {
+  const randomNumber =  Math.floor(Math.random()* 3) + 1;
     
-//     switch(randomNumber){
-//         case 1: 
-//         computerChoice = 'scissors';
-//         break;
+    switch(randomNumber){
+        case 1: 
+        computerChoice = 'scissors';
+        break;
 
-//         case 2: 
-//         computerChoice = 'rock';
-//         break;
+        case 2: 
+        computerChoice = 'rock';
+        break;
 
-//         case 3:
-//         computerChoice = 'paper';
-//         break;
-//     }
-// }
+        case 3:
+        computerChoice = 'paper';
+        break;
+    }
+}
 
-// choiceButtons.addEventListener('click',(e) => {
-//     const choice = e.target.closest('button');
-//     if(!choice) return;
+//CARD CLICK
 
-//     if(choice.classList.contains('rock_btn')) {
-//         playerChoice = 'rock';
-//         getComputerChoice();
-//         handleChoice(playerChoice,computerChoice);
-//     } else if (choice.classList.contains('paper_btn')) {
-//         playerChoice = 'paper';
-//         getComputerChoice();
-//         handleChoice(playerChoice,computerChoice);
-//     } else {
-//         playerChoice = 'scissors';
-//         getComputerChoice();
-//         handleChoice(playerChoice,computerChoice);
-//     }
-// });
+gameCards.addEventListener('click',(e) => {
+    const choice = e.target.closest('.game_card');
+    if(!choice) return;
 
-// const handleChoice = (playerChoice,computerChoice) => {
-//     if(playerChoice == 'rock' && computerChoice == 'scissors' ||
-//         playerChoice == 'scissors' && computerChoice == 'paper' ||
-//         playerChoice == 'paper' && computerChoice == 'rock'){
-//             playerScore += 1; 
-//             humanScoreOutput.textContent = playerScore;
-//             console.log("human");
-//         } else if (playerChoice == 'rock' && computerChoice == 'rock' ||
-//             playerChoice == 'scissors' && computerChoice == 'scissors' ||
-//             playerChoice == 'paper' && computerChoice == 'paper'){ 
-//                 console.log("draw");
-//         } else {
-//             computerScore += 1; 
-//             computerScoreOutput.textContent = computerScore;
-//             console.log('computer')
-//         }
-// }
+    if(choice.getAttribute("id") === 'rock') {
+        playerChoice = 'rock';
+        getComputerChoice();
+        handleChoice(playerChoice,computerChoice);
+    } else if (choice.getAttribute("id") === 'paper') {
+        playerChoice = 'paper';
+        getComputerChoice();
+     
+        handleChoice(playerChoice,computerChoice);
+    } else {
+        playerChoice = 'scissors';
+      
+        getComputerChoice();
+        handleChoice(playerChoice,computerChoice);
+    }
+});
+
+const handleChoice = (playerChoice,computerChoice) => {
+if(playerScore != 5 & computerScore != 5){
+if(playerChoice == 'rock' && computerChoice == 'scissors' ||
+        playerChoice == 'scissors' && computerChoice == 'paper' ||
+        playerChoice == 'paper' && computerChoice == 'rock'){
+            playerScore += 1; 
+
+     
+            playerScoreOutput.textContent = playerScore;
+            humanSelection.setAttribute('src', `./assets/images/${playerChoice}_winner.png`);
+            computerSelection.setAttribute('src', `./assets/images/${computerChoice}_default.png`);
+            winnerAnnouncement.textContent = 'You win!';
+            
+            checkScore();
+        } else if (playerChoice == 'rock' && computerChoice == 'rock' ||
+            playerChoice == 'scissors' && computerChoice == 'scissors' ||
+            playerChoice == 'paper' && computerChoice == 'paper'){ 
+                
+        } else {
+            computerScore += 1; 
+          
+            computerScoreOutput.textContent = computerScore;
+            humanSelection.setAttribute('src', `./assets/images/${playerChoice}_default.png`);
+            computerSelection.setAttribute('src', `./assets/images/${computerChoice}_winner.png`);
+            winnerAnnouncement.textContent = 'Computer wins!';
+            checkScore();
+        }
+} else {
+    gameFinish();
+}
+}
+
+const checkScore = () => {
+    if(playerScore == 5 || computerScore == 5){
+        gameFinish();
+    } else {
+        battleArea.classList.remove('hidden');
+        loadingEl.classList.add('hidden');
+        gameCards.classList.add('game_cards_tucked');
+        resultTimeout = setTimeout(() => {
+            battleArea.classList.add('hidden');
+            loadingEl.classList.remove('hidden');
+            gameCards.classList.remove('game_cards_tucked');
+       }, 3000);  
+       return;
+    }
+}
+
+const gameFinish = () => {
+    replaySection.classList.remove('hidden');
+
+}
